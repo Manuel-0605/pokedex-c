d62df51acf9410e1c11cfa3257327e4d820900ca
Manuel De Jesus Montañez Santoyo <manuelsantoyo177@gmail.com>
2026-03-04 14:57:19 -0600

    show pokemos on Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
 
+  const [results, setResult] = useState<any[]>([]);
+  useEffect(() => {
+    console.log("Entre en pantalla");
+    getPokemons();
+  }, []);
   const getPokemons = async () => {
     try {
-      useEffect(() => {
-        console.log("Entre en pantalla");
-        getPokemons();
-      }, []);
+
       const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
       const response = await fetch(URL, {
         method: "GET",
       });
 
       if (response.ok) {
-        console.log("Request ok")
+        const data = await response.json();
+        setResult(data.results);
       } else {
-        console.log("Bard Request")
+        console.log("Bard Request");
       }
-    } catch (error) { }
+    } catch (error) {
+      console.log("Ocurrio un error");
+    }
   };
   return (
     <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      {results.map((item) => {
+        return <Text key={item.nam}> {item.name} </Text>;
+      })}
     </View>
   );
 }
